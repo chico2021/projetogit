368aa3cf2d12918a94336d0536a05f620fe6b1e1
pool <tofikad538@ofenbuy.com>
2021-12-02 00:00:11 -0300

    alterei arquivo

--- projetogit/src/modelo/Testandogit.java
+++ projetogit/src/modelo/Testandogit.java
@@ -5,6 +5,8 @@ public class Testandogit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("fazendo alteração no arquivo");
+		//salvar o arquivo control+ s
 	}
 
 }
